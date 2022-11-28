46377b0d45aaaee4af565dfa1620ae0a7b9db493
kuan234 <114370152+kuan234@users.noreply.github.com>
2022-11-28 17:38:49 +0800

    Add files via upload

--- FYP_Project/wisteria.sql
+++ FYP_Project/wisteria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2022 at 08:06 AM
+-- Generation Time: Nov 28, 2022 at 10:38 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -43,8 +43,7 @@ CREATE TABLE `admlogin` (
 --
 
 INSERT INTO `admlogin` (`admid`, `firstname`, `lastname`, `emailaddress`, `pw`, `role_as`, `status`, `created_at`) VALUES
-(1, '', '', 'qwe@email.com', '123', 0, 0, '2022-11-02'),
-(2, '', '', 'qwe@email.com', '123', 0, 0, '2022-11-02');
+(1, 'q', 'we', 'qwe@email.com', '123', 0, 0, '2022-11-02');
 
 -- --------------------------------------------------------
 
@@ -66,14 +65,7 @@ CREATE TABLE `cart` (
 --
 
 INSERT INTO `cart` (`id`, `name`, `price`, `quantity`, `image`, `user_id`) VALUES
-(3, 'Marble Pothos', 35, 4, 'Marble pothos.PNG', 1),
-(10, 'Artifical', 20, 1, 'Artifical.jpg', 18),
-(11, 'Baby Rubber', 25, 1, 'Baby_Rubber.jpg', 18),
-(12, 'Aloe Vara', 20, 5, 'Aloe_Vara.jpg', 18),
-(13, 'Marble Pothos', 35, 4, 'Marble pothos.PNG', 18),
-(14, 'Artifical', 20, 1, 'Artifical.jpg', 1),
-(15, 'Aloe Vara', 20, 1, 'Aloe_Vara.jpg', 1),
-(16, 'Baby Rubber', 25, 1, 'Baby_Rubber.jpg', 1);
+(28, 'Artifical', 20, 1, 'Artifical.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -113,7 +105,7 @@ CREATE TABLE `user_info` (
   `address` varchar(255) NOT NULL,
   `phone` int(12) NOT NULL,
   `birthday` date NOT NULL,
-  `user_id` int(20) DEFAULT NULL,
+  `user_id` int(20) NOT NULL,
   `user_image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -122,8 +114,10 @@ CREATE TABLE `user_info` (
 --
 
 INSERT INTO `user_info` (`infoid`, `username`, `address`, `phone`, `birthday`, `user_id`, `user_image`) VALUES
-(1, 'kuan sheng zhe', '1, Jalan Bass 1, Taman 1, 21111, Melaka', 123, '2011-08-21', 1, 'shawnmendes.jpg'),
-(2, 'kuan', 'melaka', 123456789, '2001-01-01', 18, 'user.png');
+(1, 'kuan', '1, Jalan Bass 1, Taman gugua, 21111, Melaka', 123456789, '2011-08-21', 1, 'Marble pothos.PNG'),
+(2, '', '', 0, '0000-00-00', 18, 'test.png'),
+(6, 'dddddd', '1,jalan,mal', 123456789, '2222-11-25', 55, 'profile.png'),
+(7, '', '', 0, '0000-00-00', 56, NULL);
 
 -- --------------------------------------------------------
 
@@ -143,11 +137,9 @@ CREATE TABLE `user_reg` (
 
 INSERT INTO `user_reg` (`uid`, `email`, `password`) VALUES
 (1, 'email@gmail.com', '123'),
-(2, 'wqe@email.com', 'wp123!@#'),
-(3, 'lol@email.com', 'wp123!@#'),
 (18, 'kuan123@email.com', 'kuan123'),
-(19, 'kkk@email.com', 'kkk123'),
-(20, 'qwe@email.com', '123');
+(55, 'yohe@gmail.com', '123'),
+(56, 'shengzhe@gmail.com', '123');
 
 --
 -- Indexes for dumped tables
@@ -199,25 +191,25 @@ ALTER TABLE `admlogin`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `prodID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `prodID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `user_info`
 --
 ALTER TABLE `user_info`
-  MODIFY `infoid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `infoid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user_reg`
 --
 ALTER TABLE `user_reg`
-  MODIFY `uid` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `uid` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Constraints for dumped tables
