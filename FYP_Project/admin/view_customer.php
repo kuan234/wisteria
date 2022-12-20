cfb35d821be10f46f432dddfa10e7ab9be583bc1
kuan <1201202314@student.mmu.edu.my>
2022-12-20 17:32:29 +0800

    1

--- FYP_Project/admin/view_customer.php
+++ FYP_Project/admin/view_customer.php
@@ -93,7 +93,7 @@ include('connection.php');
 
     <!-- ////////////////////////////////////////////////////////////////////////////-->
 
-    <!---navbar-->
+    <!---navbar SIDEBAR-->
     <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true" data-img="theme-assets/images/backgrounds/02.jpg">
       <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">       
@@ -173,7 +173,8 @@ include('connection.php');
 
                 <?php
 
-                    }}?>
+                    }}
+                    ?>
               </tbody>
             </table>
 
