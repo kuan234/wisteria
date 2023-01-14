839eba7ed49af0af372931fa29a977020db623d0
kuan <1201202314@student.mmu.edu.my>
2023-01-14 22:49:41 +0800

    Sales Report For Each Product

--- FYP_Project/admin/reports.php
+++ FYP_Project/admin/reports.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
 include('connection.php');
-
+if(!isset($_SESSION['admin_id']))
+{
+    ?>
+    <script>window.location.href="admlogin.php"</script>
+    <?php
+}
 ?>
 
 <!DOCTYPE html>
