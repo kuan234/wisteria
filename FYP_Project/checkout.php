60731cd3b7de2413f8827967585dd612921b7a10
kuan <1201202314@student.mmu.edu.my>
2023-01-16 01:16:20 +0800

    delete promode code

--- FYP_Project/checkout.php
+++ FYP_Project/checkout.php
@@ -380,12 +380,6 @@ $count = 0;
                                     </li>
                                 
 
-                                <form class="card p-2">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Promo code">
-                                        <button type="submit" class="btn btn-secondary">Redeem</button>
-                                    </div>
-                                </form>
                             </div>
 
                            
