dc106947a81ba078790a49367571f681320604a9
kuan <1201202314@student.mmu.edu.my>
2023-01-20 16:59:10 +0800

    recent

--- FYP_Project/admin/index.php
+++ FYP_Project/admin/index.php
@@ -238,7 +238,7 @@ if(!isset($_SESSION['admin_id']))
     <div class="col-xl-4 col-lg-12">
         <div class="card">
             <div class="card-header">
-                <h4 class="card-title">Recent Buyers</h4>
+                <h4 class="card-title">Recent Customers</h4>
                 <a class="heading-elements-toggle">
                     <i class="fa fa-ellipsis-v font-medium-3"></i>
                 </a>
