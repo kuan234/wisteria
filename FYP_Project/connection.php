eb42d15d42c400ce5696f4e6c82a035bed52a71b
kuan234 <114370152+kuan234@users.noreply.github.com>
2024-02-07 13:53:46 +0800

    Update connection.php

--- FYP_Project/connection.php
+++ FYP_Project/connection.php
@@ -1,7 +1,18 @@
 <?php
 
-$connect = mysqli_connect("localhost","root","","wisteria");
+// Host Name
+$dbhost = '';
 
+// Database Name
+$dbname = 'wisteria';
+
+// Database Username
+$dbuser = 'admin';
+
+// Database Password
+$dbpass = 'admin123';
+
+$connect = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 if(!$connect)
 {
     echo("Failed to connect database.");
